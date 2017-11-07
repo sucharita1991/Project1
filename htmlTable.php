1d24ae6998e927da6ac40503b87ed6936ab0ddaa
Sucharita Das <sd686@afsconnect2.njit.edu>
2017-11-06 19:29:30 -0500

    separating table generation

--- htmlTable.php
+++ htmlTable.php
@@ -3,34 +3,25 @@
 //this class is for generating HTML table from a CSV file
 class htmlTable{
 
-    public static function genarateTableFromFile($uploadedfile,$csvFileName){
-        $formTable = '<center><h2>HTML Table Generated from CSV file: ' . $csvFileName . '</h2>';
-        $formTable .= '<table border="1" cellpadding="2" cellspacing="5">'; //generating table from the file
-        $arraylength = 0;
-        $countVar = 0;
-        while (!feof($uploadedfile)) { //looping through the file till the end of file, one row at a time.
-            $countVar++; //variable for determining the row that is being read.
+    public static function genarateTableFromFile($data,$countVar){
+
             $i = 0;
-            $data = fgetcsv($uploadedfile); //converting each line of the file to an array
             $arraylength = count($data); //counting the number of columns that the table have/ array length
             if ($countVar == 1) { //checking the first row for generating table heading.
-                $formTable .= '<tr>';
+                $csvTable = '<tr>';
                 for ($i = 0; $i < $arraylength; $i++) {
-                    $formTable .= '<th>' . $data[$i] . '</th>'; //generating table heading
+                    $csvTable .= '<th>' . $data[$i] . '</th>'; //generating table heading
                 }
-                $formTable .= '</tr>';
+                $csvTable .= '</tr>';
             } else {    //checking for the rest of the table for generating table body/table data
-                $formTable .= '<tr>';
+                $csvTable = '<tr>';
                 for ($i = 0; $i < $arraylength; $i++) {
-                    $formTable .= '<td>' . $data[$i] . '</td>'; //generating table data/rows
+                    $csvTable .= '<td>' . $data[$i] . '</td>'; //generating table data/rows
                 }
-                $formTable .= '</tr>';
+                $csvTable .= '</tr>';
             }
-        }
-        $formTable .= '</table></center>';
 
-        return $formTable;
+        return $csvTable;
     }
-
 }
 ?>
--- tableDisplay.php
+++ tableDisplay.php
@@ -10,22 +10,33 @@ class tableDisplay extends page
             $validMsg = $_SESSION['validation_message'];
         }
         $csvFileType = $_SESSION['csvFileType'];
+
         echo "<script type='text/javascript'>alert(\"$validMsg\");</script>"; //alert validation messages
 
         if ($csvFileType == "csv") { //checking for csv files before converting it into table
 
             $csvFileName = $_GET['fileName']; //retrieving uploaded file name from header
-            $formTable="";
 
-            //converting csv file to array
+            $formTable = '<center><h2>HTML Table Generated from CSV file: ' . $csvFileName . '</h2>';
+            $formTable .= '<table border="1" cellpadding="2" cellspacing="5">'; //generating table from the file
+
+            $countVar = 0;
             $data = array();
             $uploadedfile = fopen("Uploads/" . $csvFileName, "r"); //opening the file from the target directory
 
-            //generating a HTML Table from the Uploaded file
-            $formTable .= htmlTable :: genarateTableFromFile($uploadedfile,$csvFileName);
+            while (!feof($uploadedfile)) { //looping through the file till the end of file, one row at a time.
+
+                $countVar++; //variable for determining the row that is being read.
+                $data = fgetcsv($uploadedfile); //converting each line of the file to an array
+
+                //generating a HTML Table from the Uploaded file
+                $formTable .= htmlTable :: genarateTableFromFile($data,$countVar);
+            }
+            $formTable .= '</table></center>';
 
             fclose($uploadedfile); //close the opened file
             stringFunctions::printThis($formTable);  //printing table at the end
+
         }else{
             echo "Sorry, you did not upload a CSV file..!!";
         }
